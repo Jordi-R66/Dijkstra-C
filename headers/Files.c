37034157c2a05a657e27c675659b62ee4e739cf0
Jordi-R66 <66806189+Jordi-R66@users.noreply.github.com>
2025-01-30 13:06:30 +0100

    Ajout de la gestion des fichiers dans Files.c avec l'utilisation de READONLY_MODE et mise à jour des fonctions LoadVerticesFromTSV et CountLinesInFile.

--- headers/Files.c
+++ headers/Files.c
@@ -1,6 +1,7 @@
 #include "Files.h"
+#include "DijkstraTypes.h"
 
-const char READONLY_MODE = 'r';
+const char* READONLY_MODE = "r";
 const char sep = '\t';
 
 size_t CountLinesInFile(FILE* fp) {
@@ -16,11 +17,27 @@ size_t CountLinesInFile(FILE* fp) {
 		}
 	} while (c != EOF);
 
+	fseek(fp, 0, SEEK_SET);
 	return n_entries;
 }
 
 void LoadVerticesFromTSV(string filename, List* Vertices) {
+	char** endptr;
 
+	FILE* fp = fopen(filename, READONLY_MODE);
+
+	size_t n_entries = CountLinesInFile(fp);
+
+	initializeList(Vertices, n_entries, sizeof(Sommet));
+
+	uint8_t colNumber = 0;
+	s_id_t currentEntry = 0;
+
+
+
+	if (n_entries > 0) {
+
+	}
 }
 
 void LoadLinkFromTSV(string filename, List* Links) {
