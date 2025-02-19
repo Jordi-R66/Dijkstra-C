5a5f5e8bfce57722b8a1ec583cdc9261aff3068f
Jordi-R66 <66806189+Jordi-R66@users.noreply.github.com>
2025-02-19 11:48:21 +0100

    Ajout de la fonction freeAll pour libérer un tableau de pointeurs et réorganisation des inclusions de fichiers dans commons.h

--- headers/commons.h
+++ headers/commons.h
@@ -4,7 +4,9 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include <stdbool.h>
-#include "string.h"
+#include <string.h>
 
 typedef char* string;
 typedef int64_t s_id_t;
+
+void freeAll(void** __ptrs, size_t __n_ptrs);
