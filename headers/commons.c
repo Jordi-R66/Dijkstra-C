16c0a4e8df6bb84de5e30596df1e8136b3476919
Jordi-R66 <66806189+Jordi-R66@users.noreply.github.com>
2025-02-19 11:56:27 +0100

    Ajout d'une vérification de nullité avant la libération de mémoire dans la fonction freeAll pour éviter des erreurs de libération.

--- headers/commons.c
+++ headers/commons.c
@@ -2,6 +2,10 @@
 
 void freeAll(void** __ptrs, size_t __n_ptrs) {
 	for (size_t i = 0; i < __n_ptrs; i++) {
-		free(__ptrs[i]);
+		void* ptr = __ptrs[i];
+
+		if (ptr != NULL) {
+			free(__ptrs[i]);
+		}
 	}
 }
