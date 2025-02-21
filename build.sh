a321283867823db6c99254bd97f8cab63caec2c7
Jordi-R66 <66806189+Jordi-R66@users.noreply.github.com>
2025-02-21 16:58:59 +0100

    Modification de l'option de compilation dans le script build.sh pour utiliser la syntaxe Intel en assembleur.

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-gcc -std=c23 main.c headers/commons.c headers/DijkstraTypes.c headers/Files.c headers/list.c -o releases/Dijkstra.elf
+gcc -masm=intel -std=c23 main.c headers/commons.c headers/DijkstraTypes.c headers/Files.c headers/list.c -o releases/Dijkstra.elf
