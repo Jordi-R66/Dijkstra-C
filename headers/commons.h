b7068aa844a70ff9381576706aa08682a593cfa9
Jordi-R66 <66806189+Jordi-R66@users.noreply.github.com>
2025-02-21 14:14:57 +0100

    Mise à jour de la norme C à C23 dans les fichiers de configuration et ajout de la gestion conditionnelle de l'inclusion de stdbool dans commons.h.

--- headers/commons.h
+++ headers/commons.h
@@ -3,8 +3,13 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
-#include <stdbool.h>
 #include <string.h>
+
+#if defined __STDC_VERSION__ && __STDC_VERSION__ > 201710L
+/* bool, true and false are keywords.  */
+#else
+#include <stdbool.h>
+#endif
 
 typedef char* string;
 typedef int64_t s_id_t;
