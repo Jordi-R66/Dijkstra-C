7815ec06e2588eb8dc959773235492cbc2467afc
Jordi-R66 <66806189+Jordi-R66@users.noreply.github.com>
2025-02-27 14:37:51 +0100

    Modification des options de compilation pour désactiver l'optimisation et activer le débogage dans les scripts de construction.

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-gcc -O3 -s -masm=intel -std=c23 main.c headers/commons.c headers/DijkstraTypes.c headers/Files.c headers/list.c -o releases/Dijkstra.elf
+gcc -O0 -g -masm=intel -std=c23 main.c headers/commons.c headers/DijkstraTypes.c headers/Files.c headers/list.c -o releases/Dijkstra.elf
