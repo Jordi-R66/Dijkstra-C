33dc29b50fd6a6fe41f8746dc907e1bdb036b15d
Jordi-R66 <66806189+Jordi-R66@users.noreply.github.com>
2025-02-27 13:29:37 +0100

    Ajout des options de compilation -O0 et -g pour le débogage dans le script de construction.

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-gcc -masm=intel -std=c23 main.c headers/commons.c headers/DijkstraTypes.c headers/Files.c headers/list.c -o releases/Dijkstra.elf
+gcc -O0 -g -masm=intel -std=c23 main.c headers/commons.c headers/DijkstraTypes.c headers/Files.c headers/list.c -o releases/Dijkstra.elf
