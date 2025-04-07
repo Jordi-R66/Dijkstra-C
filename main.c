b0b6e66c2ec5ee6d13abedf7e59fa33c017f17bb
Jordi-R66 <66806189+Jordi-R66@users.noreply.github.com>
2025-04-07 23:25:05 +0200

    Renommage des variables de fichiers pour une meilleure clarté et ajout de l'initialisation de la liste des arêtes.

--- main.c
+++ main.c
@@ -4,13 +4,20 @@
 #include "headers/Files.h"
 
 int main(void) {
-	string filename = "France_s.csv";
+	string sommetsFilename = "France_s.csv";
+	string liensFilename = "France_l.csv";
 
 	List Vertices;
-	Vertices.initialized = false;
+	List Edges;
 
-	printf("Loading vertices in %lX\n", (uint64_t)&Vertices);
-	LoadVerticesFromTSV(filename, &Vertices);
+	Vertices.initialized = false;
+	Edges.initialized = false;
+
+	//printf("Loading vertices in %lX\n", (uint64_t)&Vertices);
+	//LoadVerticesFromTSV(sommetsFilename, &Vertices);
+
+	printf("Loading edges in %lX\n", (uint64_t)&Edges);
+	LoadLinkFromTSV(liensFilename, &Edges);
 
 	printf("Vertices loaded in %lX\n", (uint64_t)&Vertices);
 	freeSommetList(&Vertices);
