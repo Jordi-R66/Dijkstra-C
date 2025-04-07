a77104ee794026090f5dc90aee99f3d2c492f450
Jordi-R66 <66806189+Jordi-R66@users.noreply.github.com>
2025-04-07 22:41:04 +0200

    Ajout d'une énumération Sommet_Fields pour définir les champs des sommets dans le fichier d'en-tête Files.h.

--- headers/Files.h
+++ headers/Files.h
@@ -3,5 +3,13 @@
 #include "commons.h"
 #include "list.h"
 
+typedef enum {
+	SOMMET_ID = 0,
+	SOMMET_NAME = 1,
+	SOMMET_X = 2,
+	SOMMET_Y = 3,
+	SOMMET_Z = 4
+} Sommet_Fields;
+
 void LoadVerticesFromTSV(string filename, List* Vertices);
 void LoadLinkFromTSV(string filename, List* Links);
