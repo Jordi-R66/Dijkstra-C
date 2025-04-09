8dea02cb4d0182008fd66852f27f709b467f2256
Jordi-R66 <66806189+Jordi-R66@users.noreply.github.com>
2025-04-09 13:20:56 +0200

    Ajout de la fonction initDict pour initialiser un dictionnaire avec des clés et des valeurs.

--- headers/dict.c
+++ headers/dict.c
@@ -1,1 +1,8 @@
 #include "dict.h"
+
+void initDict(Dict* dict, size_t keySize, size_t valSize, size_t initCapacity) {
+	initializeList(&dict->keys, initCapacity, keySize);
+	initializeList(&dict->values, initCapacity, valSize);
+
+	dict->capacity = initCapacity;
+}
--- headers/dict.h
+++ headers/dict.h
@@ -5,8 +5,10 @@
 typedef struct {
 	List keys;
 	List values;
+	size_t capacity;
 } Dict;
 
+void initDict(Dict* dict, size_t keySize, size_t valSize, size_t n_records);
 void* getValue(void* key);
 void addValue(void* key, void* value);
 void removeValue(void* key);
